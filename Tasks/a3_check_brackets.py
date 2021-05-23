1db9cf959db940c7da8be95009206902db942ff0
Grigorii <dvraz1988@gmail.com>
2021-05-23 22:37:14 +0300

    a3_check_brackets done

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -1,3 +1,6 @@
+import re
+
+
 def check_brackets(brackets_row: str) -> bool:
     """
     Check whether input string is a valid bracket sequence
@@ -5,5 +8,11 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-
+    a = "".join([i for i in brackets_row if i == "(" or i == ")"])
+    if a == "":
+        return True
+    for i in range(len(a)):
+        if not a:
+            return True
+        a = re.sub(r"\(\)", "", a)
     return False
